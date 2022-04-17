2a762842a89b4dd0fdb1253a71ca1b2b39397892
Kikilma <kikilma.tech@gmail.com>
2022-04-17 16:48:34 -0600

    correccion

--- install.sh
+++ install.sh
@@ -277,8 +277,8 @@ sudo ./install_docker.sh
 sudo rm install_docker.sh
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo git clone https://github.com/ioticos/ioticos_god_level_services.git
-sudo mv ioticos_god_level_services services
+sudo git clone https://github.com/Kikilma/k1srv.git
+sudo mv k1srv services
 
 
 cd services
@@ -304,8 +304,8 @@ sudo sh -c " echo 'EMQX_DEFAULT_USER_PASSWORD=${EMQX_DEFAULT_USER_PASSWORD}' >> 
 sudo sh -c " echo 'EMQX_DEFAULT_APPLICATION_SECRET=${EMQX_DEFAULT_APPLICATION_SECRET}' >> $filename"
 
 
-sudo git clone https://github.com/ioticos/ioticos_god_level_app.git
-sudo mv ioticos_god_level_app  app
+sudo git clone https://github.com/Kikilma/k1app.git
+sudo mv k1app  app
 
 cd app
 
